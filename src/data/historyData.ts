ecdd18d9f090a7f12555737d28997a3733d3800c
Mia60codes <miasitini@gmail.com>
2026-06-17 13:45:58 +0300

    Update historyData.ts

--- src/data/historyData.ts
+++ src/data/historyData.ts
@@ -17,213 +17,42 @@ export interface HistoryDay {
 
 export const historyDays: HistoryDay[] = [
 
-    {
-    date: "07-06-2026",
-    totalOdds: "21.61",
+  {
+    date: "17-06-2026",
+    totalOdds: "27.00",
     matches: [
+      { homeTeam: "Locked 🔐", awayTeam: "Locked 🔐", tip: "Draw FT", odds: "3.05", status: "PENDING" },
+      { homeTeam: "Locked 🔐", awayTeam: "Locked 🔐", tip: "Draw FT", odds: "3.05", status: "PENDING" },
       { homeTeam: "Locked 🔐", awayTeam: "Locked 🔐", tip: "Draw FT", odds: "3.00", status: "PENDING" },
-      { homeTeam: "Locked 🔐", awayTeam: "Locked 🔐", tip: "Draw FT", odds: "2.95", status: "PENDING" },
-      { homeTeam: "Locked 🔐", awayTeam: "Locked 🔐", tip: "Draw FT", odds: "3.00", status: "PENDING" },
-    ]
-  },
-
-
-
-{
-    date: "06-06-2026",
-    totalOdds: "20.90",
-    matches: [
-      { homeTeam: "Acasusso", awayTeam: "Central Note", tip: "Draw FT", odds: "2.89", status: "LOST" },
-      { homeTeam: "Los Andes", awayTeam: "CA Estudiantes", tip: "Draw FT", odds: "3.00", status: "WON" },
-      { homeTeam: "San Miguel", awayTeam: "Belgrano", tip: "Draw FT", odds: "3.10", status: "WON" },
-    ]
-  },
-
-
-{
-    date: "05-06-2026",
-    totalOdds: "23.61",
-    matches: [
-      { homeTeam: "Kabylie", awayTeam: "Belouizdad", tip: "Draw FT", odds: "3.20", status: "WON" },
-      { homeTeam: "Mostaganem", awayTeam: "El Bayadh", tip: "Draw FT", odds: "2.95", status: "WON" },
-      { homeTeam: "Angola", awayTeam: "Mauritania", tip: "Draw FT", odds: "3.10", status: "WON" },
     ]
   },
 
   {
-    date: "04-06-2026",
-    totalOdds: "21.00",
+    date: "16-06-2026",
+    totalOdds: "19.31",
     matches: [
-      { homeTeam: "Goree", awayTeam: "Mbour", tip: "Draw FT", odds: "2.80", status: "WON" },
-      { homeTeam: "Welayta Dicha", awayTeam: "Negele Arsi", tip: "Draw FT", odds: "2.90", status: "WON" },
-      { homeTeam: "Paraguari", awayTeam: "Tembetary", tip: "Draw FT", odds: "3.00", status: "WON" },
+      { homeTeam: "Ethio Electric", awayTeam: "Makelle 70", tip: "Draw FT", odds: "3.00", status: "WON" },
+      { homeTeam: "Sheger Ketema", awayTeam: "Welwalo", tip: "Draw FT", odds: "2.80", status: "WON" },
+      { homeTeam: "Shahrdari", awayTeam: "Navad Urmia", tip: "Draw FT", odds: "2.90", status: "WON" },
     ]
   },
 
   {
-    date: "03-06-2026",
-    totalOdds: "8.85",
+    date: "15-06-2026",
+    totalOdds: "11.23",
     matches: [
-      { homeTeam: "Atletico FC", awayTeam: "Nueva", tip: "Draw FT", odds: "2.80", status: "WON" },
-      { homeTeam: "Brescia", awayTeam: "Ascoli", tip: "Draw FT", odds: "2.90", status: "WON" },
+      { homeTeam: "Belgium", awayTeam: "Egypt", tip: "Draw FT", odds: "4.00", status: "WON" },
+      { homeTeam: "Atl. Rafaela", awayTeam: "Drp. Riestra", tip: "Draw FT", odds: "3.00", status: "WON" },
     ]
   },
 
   {
-    date: "02-06-2026",
-    totalOdds: "9.01",
+    date: "14-06-2026",
+    totalOdds: "21.31",
     matches: [
-      { homeTeam: "Belouizdad", awayTeam: "USM Alger", tip: "Draw FT", odds: "3.00", status: "WON" },
-      { homeTeam: "Eslamshahr", awayTeam: "Shahrdari", tip: "Draw FT", odds: "2.70", status: "WON" },
-    ]
-  },
-
-  {
-    date: "01-06-2026",
-    totalOdds: "11.29",
-    matches: [
-      { homeTeam: "Ponte Preta", awayTeam: "Botafogo SP", tip: "Draw FT", odds: "3.10", status: "WON" },
-      { homeTeam: "Al Masry", awayTeam: "ZED", tip: "Draw FT", odds: "3.00", status: "WON" },
-    ]
-  },
-
-  {
-    date: "31-05-2026",
-    totalOdds: "20.20",
-    matches: [
-      { homeTeam: "Madryn", awayTeam: "Acassuso", tip: "Draw FT", odds: "2.90", status: "WON" },
-      { homeTeam: "Constanta", awayTeam: "Chindia", tip: "Draw FT", odds: "3.00", status: "WON" },
-      { homeTeam: "Patronato", awayTeam: "Tristan Suarez", tip: "Draw FT", odds: "2.90", status: "WON" },
-    ]
-  },
-
-  {
-    date: "30-05-2026",
-    totalOdds: "21.53",
-    matches: [
-      { homeTeam: "Makelle 70", awayTeam: "Bahir Dar kenema", tip: "Draw FT", odds: "2.90", status: "WON" },
-      { homeTeam: "Welwalo", awayTeam: "Ethiopian Insuarance", tip: "Draw FT", odds: "3.00", status: "WON" },
-      { homeTeam: "FUS Rabat", awayTeam: "FAR Rabat", tip: "Draw FT", odds: "3.20", status: "WON" },
-    ]
-  },
-
-  {
-    date: "29-05-2026",
-    totalOdds: "21.66",
-    matches: [
-      { homeTeam: "National Egypt", awayTeam: "Al Ittihad", tip: "Draw FT", odds: "2.95", status: "WON" },
-      { homeTeam: "Fasil Kenema", awayTeam: "Sheger Ketema", tip: "Draw FT", odds: "2.90", status: "WON" },
-      { homeTeam: "Ethiopia Nighd", awayTeam: "Ethio Electick", tip: "Draw FT", odds: "4.00", status: "WON" },
-    ]
-  },
-
-  {
-    date: "28-05-2026",
-    totalOdds: "22.88",
-    matches: [
-      { homeTeam: "River Plate 2", awayTeam: "San lorenzo", tip: "Draw FT", odds: "3.10", status: "WON" },
-      { homeTeam: "Rio Branco", awayTeam: "Vilavelhense", tip: "Draw FT", odds: "3.00", status: "WON" },
-      { homeTeam: "Minaaa", awayTeam: "Al Talaba", tip: "Draw FT", odds: "3.05", status: "WON" },
-    ]
-  },
-
-  {
-    date: "27-05-2026",
-    totalOdds: "20.07",
-    matches: [
-      { homeTeam: "Niyore Zamini", awayTeam: "Havadar", tip: "Draw FT", odds: "3.00", status: "WON" },
-      { homeTeam: "Mara sugar", awayTeam: "Gor Mahia", tip: "Draw FT", odds: "2.80", status: "WON" },
-      { homeTeam: "Kibera", awayTeam: "Nairobi City", tip: "Draw FT", odds: "3.00", status: "WON" },
-    ]
-  },
-
-  {
-    date: "26-05-2026",
-    totalOdds: "18.13",
-    matches: [
-      { homeTeam: "Frandria", awayTeam: "Arsenal Sarandi", tip: "Draw FT", odds: "2.95", status: "WON" },
-      { homeTeam: "Sidama Bunna", awayTeam: "Ethiopian Insuarance", tip: "Draw FT", odds: "2.70", status: "WON" },
-      { homeTeam: "Deportivo Merlo", awayTeam: "Liniers", tip: "Draw FT", odds: "2.90", status: "WON" },
-    ]
-  },
-
-  {
-    date: "25-05-2026",
-    totalOdds: "23.90",
-    matches: [
-      { homeTeam: "Enppi", awayTeam: "Wadi Degla", tip: "Draw FT", odds: "3.00", status: "WON" },
-      { homeTeam: "Welwalo", awayTeam: "Makelle 70", tip: "Draw FT", odds: "3.00", status: "WON" },
-      { homeTeam: "Derry City", awayTeam: "Shelbourne", tip: "Draw FT", odds: "3.10", status: "WON" },
-    ]
-  },
-
-  {
-    date: "24-05-2026",
-    totalOdds: "24.96",
-    matches: [
-      { homeTeam: "Fiorentina", awayTeam: "Roma", tip: "Draw FT", odds: "3.10", status: "WON" },
-      { homeTeam: "Tombense", awayTeam: "Botafogo SP", tip: "Draw FT", odds: "3.20", status: "WON" },
-      { homeTeam: "Tigre", awayTeam: "San Lorenzo", tip: "Draw FT", odds: "2.90", status: "WON" },
-    ]
-  },
-
-  {
-    date: "23-05-2026",
-    totalOdds: "21.00",
-    matches: [
-      { homeTeam: "Nueva Chicago", awayTeam: "Temperley", tip: "Draw FT", odds: "2.90", status: "WON" },
-      { homeTeam: "Armenio", awayTeam: "Dock Sud", tip: "Draw FT", odds: "2.80", status: "WON" },
-      { homeTeam: "Modern Sport", awayTeam: "Ghazl EL Mahallah", tip: "Draw FT", odds: "2.90", status: "WON" },
-    ]
-  },
-
-  {
-    date: "22-05-2026",
-    totalOdds: "24.82",
-    matches: [
-      { homeTeam: "Fiorentina", awayTeam: "Atalanta", tip: "Draw FT", odds: "3.20", status: "WON" },
-      { homeTeam: "Arsenal Sarandi", awayTeam: "Villa San Carlos", tip: "Draw FT", odds: "3.20", status: "WON" },
-      { homeTeam: "Sudtirol", awayTeam: "Bari", tip: "Draw FT", odds: "3.30", status: "WON" },
-    ]
-  },
-
-  {
-    date: "21-05-2026",
-    totalOdds: "19.61",
-    matches: [
-      { homeTeam: "Barracas", awayTeam: "Union De Santa", tip: "Draw FT", odds: "3.30", status: "WON" },
-      { homeTeam: "Wadi Degla", awayTeam: "ZED", tip: "Draw FT", odds: "2.90", status: "WON" },
-      { homeTeam: "Adama City", awayTeam: "Welwalo Adigrat", tip: "Draw FT", odds: "2.80", status: "WON" },
-    ]
-  },
-
-  {
-    date: "20-05-2026",
-    totalOdds: "20.81",
-    matches: [
-      { homeTeam: "Ethio Electric", awayTeam: "Dire Dawa", tip: "Draw FT", odds: "2.90", status: "WON" },
-      { homeTeam: "Dodoma Jiji", awayTeam: "Mashujaa", tip: "Draw FT", odds: "3.10", status: "WON" },
-      { homeTeam: "Kabwe", awayTeam: "Zanaco", tip: "Draw FT", odds: "3.00", status: "WON" },
-    ]
-  },
-
-  {
-    date: "19-05-2026",
-    totalOdds: "24.61",
-    matches: [
-      { homeTeam: "Banfield 2", awayTeam: "Aldosivi 2", tip: "Draw FT", odds: "3.20", status: "WON" },
-      { homeTeam: "Constantine", awayTeam: "Khenchela", tip: "Draw FT", odds: "3.20", status: "WON" },
-      { homeTeam: "Hadiya Hossana", awayTeam: "Ethiopia Nigd Bank", tip: "Draw FT", odds: "2.80", status: "WON" },
-    ]
-  },
-
-  {
-    date: "18-05-2026",
-    totalOdds: "19.63",
-    matches: [
-      { homeTeam: "Belgrano", awayTeam: "Chaco For Ever", tip: "Draw FT", odds: "3.00", status: "WON" },
-      { homeTeam: "Italiano", awayTeam: "Laferrere", tip: "Draw FT", odds: "2.80", status: "WON" },
-      { homeTeam: "Arab Contractor", awayTeam: "Wadi Degla", tip: "Draw FT", odds: "2.90", status: "WON" },
+      { homeTeam: "Netherlands", awayTeam: "Japan", tip: "Draw FT", odds: "3.16", status: "WON" },
+      { homeTeam: "Kimberley", awayTeam: "Alvarado", tip: "Draw FT", odds: "2.90", status: "WON" },
+      { homeTeam: "Quilmes", awayTeam: "Armenio", tip: "Draw FT", odds: "3.05", status: "WON" },
     ]
   },
 
